c742522832bdec8dd73f0a9c5e99180ee8e2d9f8
Ahmed Elhosseiny <ahmed.elhosseiny@Ahmeds-MacBook-Pro.local>
2018-04-19 21:57:16 -0700

    Initial Commit

---
+++ ScratchPadCode/ScratchPadCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ScratchPadCode
+//
+//  Created by Ahmed Elhosseiny on 4/19/18.
+//  Copyright © 2018 Ahmed Elhosseiny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
