9a20100b3bf2d721b2ae9e67eeecb74f56a76a0f
Ahmed Elhosseiny <aelhosse@qti.qualcomm.com>
2018-05-02 18:43:57 -0700

    Solved Longest Substring Without Repeating Characters

--- LongestSubstringWithoutRepeatingCharacters/main/main/main.cpp
+++ LongestSubstringWithoutRepeatingCharacters/main/main/main.cpp
@@ -7,19 +7,18 @@
 #include <unordered_set>
 #include <string>
 
-#define DEBUG 0
+#define DEBUG 1
 
 using namespace std;
 
 int main()
 {
     string str = "abcabcbb", input = "", maxString = "";
-    unordered_map<string, int> strMap;
     unordered_map<string, int>::iterator mapIter;
 
     unordered_set<char> strSet;
 
-    int walker = 0, max = 0;
+    int walker = 0, count = 0, max = 0;
     for (int i = 0; i < str.size(); i++) {
         walker = i;
         while (strSet.find(str[walker]) == strSet.end() && walker < str.size()) {
@@ -27,24 +26,17 @@ int main()
             if (DEBUG) cout << "Adding " << str[walker] << " to the set." << endl;
             input.push_back(str[walker]);
             walker++;
+            count++;
+            if (max < count){
+                max = count;
+                maxString = input;
+            }
         }
         if(DEBUG) cout << "Found a duplicate char, clearing set.\n";
 
-        mapIter = strMap.find(input);
-        if (mapIter != strMap.end()) {
-            mapIter->second += 1;
-            if (DEBUG) cout << "Found string: " << input << " in the map." << endl;
-            if (max < mapIter->second) {
-                maxString = input;
-                max = mapIter->second;
-            }
-        }
-        else {
-            strMap.insert(make_pair(input, 1));
-            if (DEBUG) cout << "Adding string: " << input << " to the map." << endl;
-        }
         strSet.clear();
         input = "";
+        count = 0;
 
     }
 
